e9c89b82bd485f842c9896475df576bafc49b174
Liroy99 <mingjanssen999@gmail.com>
2025-12-06 19:33:23 +0100

    part 1

--- client/src/app/pages/landing/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-export { useWindowSize } from './useWindowSize';
-export type { WindowSize } from './useWindowSize';
-export { useThrottle } from './useThrottle';
-export { useDebounce } from './useDebounce';
-
